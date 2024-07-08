f4b47afa758eff8599673e095681868c8eba7144
alex@example.com <samuray150305@gmail.com>
2024-07-08 14:07:52 +0300

    code: именён вормат вывода, добавлен вывод разности a и b

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << a + b << endl;
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n';
 }
